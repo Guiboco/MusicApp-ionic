1f7801ecdcd325428aef6fca622de9d4f1aec687
Guillermo <guiboco18@gmail.com>
2019-10-09 21:50:48 +0200

    created variable instrumentos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  instrumentos = [
+     {
+       nombre: "flauta",
+       imagen: "assets/imagenes/flauta.png",
+       audio: "assets/sonidos/flauta.mp3"
+     },
+     {
+      nombre: "bombo1",
+      imagen: "assets/imagenes/bombo1.png",
+      audio: "assets/sonidos/bombo1.mp3"
+    },
+    {
+      nombre: "bombo2",
+      imagen: "assets/imagenes/bombo2.png",
+      audio: "assets/sonidos/bombo2.mp3"
+    },
+    {
+      nombre: "platillos",
+      imagen: "assets/imagenes/platillos.png",
+      audio: "assets/sonidos/platillos.mp3"
+    }
+  ]
   constructor() {}
 
 }
