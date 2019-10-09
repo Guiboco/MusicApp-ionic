3798dfad34d2b04704ff1122d97a1f74890bf5c1
Guillermo <guiboco18@gmail.com>
2019-10-09 21:59:36 +0200

    variable reproducirSonido created

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   instrumentos = [
-     {
-       nombre: "flauta",
-       imagen: "assets/imagenes/flauta.png",
-       audio: "assets/sonidos/flauta.mp3"
-     },
-     {
+    {
+      nombre: "flauta",
+      imagen: "assets/imagenes/flauta.png",
+      audio: "assets/sonidos/flauta.mp3"
+    },
+    {
       nombre: "bombo1",
       imagen: "assets/imagenes/bombo1.png",
       audio: "assets/sonidos/bombo1.mp3"
@@ -28,6 +28,11 @@ export class HomePage {
       audio: "assets/sonidos/platillos.mp3"
     }
   ]
-  constructor() {}
 
+  reproducirSonido(instrumento) {
+    let sonido = new Audio();
+    sonido.src = instrumento.audio;
+    sonido.load();
+    sonido.play();
+  }
 }
